8220f0fe54dbf0a362f91f5d78ef5d91a9f2d14b
devify2022 <2022devify@gmail.com>
2025-01-19 23:53:34 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
-import http from "http";
+import https from "https";  // Import https module
+import fs from "fs";       // Import fs to read certificate files
 import cors from "cors";
 import errorHandler from "../src/middlewares/error.middleware.js";
 import { Server } from "socket.io";
@@ -29,33 +30,33 @@ import productCategoryRoutes from "../src/routes/product/productcategory.routes.
 import productRoutes from "../src/routes/product/product.routes.js";
 import orderRoutes from "../src/routes/product/order.routes.js";
 
-
 // API routes
 app.use("/astrobandhan/v1/user", userRouter);
 app.use("/astrobandhan/v1/admin", adminRouter);
 app.use("/astrobandhan/v1/astrologer", astrologerRouter);
-
-
 app.use("/astrobandhan/v1/productCategory", productCategoryRoutes);
 app.use("/astrobandhan/v1/product", productRoutes);
 app.use("/astrobandhan/v1/order", orderRoutes);
 
-
 // Error handling middleware
 app.use(errorHandler);
 
-// Create the HTTP server
-const server = http.createServer(app);
+// Read the self-signed certificate and private key from the correct paths
+const privateKey = fs.readFileSync("/etc/ssl/private/selfsigned.key", "utf8");
+const certificate = fs.readFileSync("/etc/ssl/private/selfsigned.crt", "utf8");
 
+// Create HTTPS server
+const credentials = { key: privateKey, cert: certificate };
+const server = https.createServer(credentials, app);
 
 // Initialize socket
 initSocket(server);  // This initializes the socket.io server
 
 // Start the server on port 6000
-const PORT = process.env.PORT || 6000; // Default to 6000 if not provided
+const PORT =  8080; // Default to 6000 if not provided
 server.listen(PORT, () => {
-  const wsUrl = `ws://localhost:${PORT}`;  // WebSocket URL for testing
-  console.log(`AstroBandhan is running on http://localhost:${PORT}`);
+  const wsUrl = `wss://localhost:${PORT}`;  // WebSocket URL for testing with wss (secure WebSocket)
+  console.log(`AstroBandhan is running on https://localhost:${PORT}`);
   console.log(`WebSocket server is running at: ${wsUrl}`);
 });
 
