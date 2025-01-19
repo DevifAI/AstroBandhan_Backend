9b6311cbb799e89e2748412ae2ee1cde7eb2b4ff
devify2022 <2022devify@gmail.com>
2025-01-19 23:50:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 // index.js
-import fs from "fs";
-import https from "https";
 import dotenv from "dotenv";
 import { app } from "./app.js";
 import connectDB from "./db/index.js";
@@ -10,23 +8,12 @@ dotenv.config({
   path: "../env",
 });
 
-const options = {
-  key: fs.readFileSync("/etc/ssl/private/selfsigned.key"),
-  cert: fs.readFileSync("/etc/ssl/private/selfsigned.crt"),
-};
 
 
 
 connectDB()
   .then(() => {
     console.log("MONGO DB CONNECTION Done !!!");
-
-    // Create HTTPS server
-    https
-      .createServer(options, app)
-      .listen(process.env.PORT, () => {
-        console.log(`Server running on https://localhost:${process.env.PORT}`);
-      });
   })
   .catch((err) => {
     console.log("MONGO DB CONNECTION FAILED !!!", err);
