dc6826d6ef287d974dd51530326aab3d3dfb4e7d
Subhojit Dutta <89318927+uiSubho98@users.noreply.github.com>
2025-05-24 13:56:31 +0530

    t

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 // index.js
 import dotenv from "dotenv";
-import { app } from "./app.js";
-import connectDB from "./db/index.js";
+import connectDB from "./src/db/index.js";
 
-
-dotenv.config({
-  path: "../env",
-});
-
-
-
+dotenv.config({ path: ".env" });
 
 connectDB()
   .then(() => {
